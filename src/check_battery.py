93738232266e66e1fb6655dce11ae704e831e2c1
json-Liang <yuanyu90221@gmail.com>
2019-10-17 17:54:55 +0800

    change win10toast to plyer

--- src/check_battery.py
+++ src/check_battery.py
@@ -1,15 +1,14 @@
-from win10toast import ToastNotifier
+from plyer import notification
 import psutil
 
 battery = psutil.sensors_battery()
 charging = battery.power_plugged
 percent = battery.percent
 message = '目前電池電量為 ' + str(percent) +'%'
-# One-time initialization
-toaster = ToastNotifier()
+
 if percent > 40 and not charging:
     print(message)
 if percent <= 40 and not charging:
     print('電量不足，請充電'+ message)
 elif percent >= 80 and charging:
-    toaster.show_toast('電量充足，請拔掉電源線'+message, "alert!", threaded=True, icon_path=None, duration=3)
+    notification.notify( title="alert!",message='電量充足，請拔掉電源線'+message, app_icon=None, timeout=10)
