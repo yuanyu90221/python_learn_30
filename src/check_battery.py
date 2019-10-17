27d4d54cd57d8c5971f22d2f9b2428402313c58f
json-Liang <yuanyu90221@gmail.com>
2019-10-17 19:59:33 +0800

    update usage of check batter

--- src/check_battery.py
+++ src/check_battery.py
@@ -7,7 +7,7 @@ percent = battery.percent
 message = '目前電池電量為 ' + str(percent) +'%'
 
 if percent > 40 and not charging:
-    print(message)
+    notification.notify(title='full mode',message=message, app_icon=None, timeout=10)
 if percent <= 40 and not charging:
     print('電量不足，請充電'+ message)
 elif percent >= 80 and charging:
