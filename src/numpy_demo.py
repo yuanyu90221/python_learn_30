e1e6fd7281b604311ad864aa7a786687c411caa9
json-Liang <yuanyu90221@gmail.com>
2019-10-18 23:30:41 +0800

    numpy array

---
+++ src/numpy_demo.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+array_a = np.array([1, 2, 3, 4, 5])
+
+print(array_a)
+
+print([1, 2, 3, 4, 5, "json"])
+
+print(array_a.shape)
+
+array_b = np.array([[1,2,3,4,5],[1,2,3,4,5]])
+
+print(array_b.shape)
