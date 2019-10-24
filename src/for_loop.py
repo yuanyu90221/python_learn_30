679841f140e223250701072953fe6bbb1e0bf87e
yuanyu90221 <yuanyu90221@gmail.com>
2019-10-25 02:15:22 +0800

    foor_loop

---
+++ src/for_loop.py
@@ -0,0 +1,14 @@
+name = "Derrick"
+
+for element in name:
+    print(element)
+
+list_a = ["Derrick", "michael", "Jennifer", 3,6.5]
+
+for name in list_a:
+    print(name)
+
+list_b = [1,2,3,4,5]
+
+for number in list_b:
+    print(number+5)
