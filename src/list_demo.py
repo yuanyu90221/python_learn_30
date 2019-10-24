c3717bfb608a8e13355f9f988ced02478e01f1b2
yuanyu90221 <yuanyu90221@gmail.com>
2019-10-25 01:06:30 +0800

    list demo

---
+++ src/list_demo.py
@@ -0,0 +1,19 @@
+our_list = ["a", "b", "c", 1, 1.5]
+print(our_list)
+
+our_list.append("d")
+print(our_list)
+
+print(our_list[0])
+
+our_list.insert(0,"First element")
+print(our_list)
+
+our_list.remove("First element")
+print(our_list)
+
+our_list.pop(5)
+print(our_list)
+
+nested_list = [[1,2,3], 3, 4, 5]
+print(nested_list)
