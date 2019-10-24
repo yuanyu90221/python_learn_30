c2fe80c2b9f3a8f206ad13469b6a78ee8ff58ae1
yuanyu90221 <yuanyu90221@gmail.com>
2019-10-25 00:47:08 +0800

    update

---
+++ src/complex_dataType.py
@@ -0,0 +1,17 @@
+# list, tuple, dictionary, frozenset, set
+
+our_list = ["a","b", "c", 1.5]
+
+print(our_list)
+
+our_tuple = ("a", "b", "c")
+print(our_tuple)
+
+our_dictionary = {"name":"json","height":"6.0"}
+print(our_dictionary)
+
+our_set = {"one", "two", "three"}
+print(our_set)
+
+our_frozenset = frozenset(our_set)
+print(our_frozenset)
---
+++ src/sample_val.py
@@ -0,0 +1,8 @@
+age = "1"
+print(type(age))
+
+number = float(1)
+print(type(number))
+
+number_and_age = (number, age)
+print(number_and_age)
